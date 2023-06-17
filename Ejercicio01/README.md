7c5b315655b34d951616651e1d4e3e88d3172d88
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-06-17 04:06:25 -0500

    Se agrega una carpeta personalizada con el proyecto del peorcaso Grafico

---
+++ Ejercicio01/README.md
@@ -0,0 +1,1 @@
+Para ejecutar de forma correcta la forma gráfica, es mejor el uso de un IDE como Netbeans o Eclipse, además, se incluye en el código la librería de JavaPlot
---
+++ Ejercicio01/peorCasoGrafico/src/main/java/com/mycompany/peorCasoGrafico/List.java
@@ -0,0 +1,68 @@
+public class List<T> {
+
+    private Node<T> head;
+    private int count;
+
+    public List() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+
+    public int length() {
+        return this.count;
+    }
+
+    public void insert(T x) {
+        if (isEmpty()) {
+            this.head = new Node<T>(x, this.head);
+        } 
+        else {
+            Node<T> aux = this.head;
+            while (aux.getNext() != null) {
+                aux = aux.getNext();
+            }
+            aux.setNext(new Node<T>(x));
+        }
+        this.count++;
+    }
+
+    public boolean search(T x) {
+        Node<T> aux = this.head;
+        for (; aux != null && !aux.getData().equals(x); aux = aux.getNext());
+        return aux != null;
+    }
+
+    public Node<T> get(int p) {
+        Node<T> aux = this.head;
+        for (int i = 0; i < p; i++) {
+            aux = aux.getNext();
+        }
+        return aux;
+    }
+
+    public void remove(int p) {
+        if (p < count) {
+            if (p == 0) {
+                this.head = this.head.getNext();
+            } 
+            else {
+                Node<T> anterior = this.get(p - 1);
+                anterior.setNext(this.get(p + 1));
+            }
+            this.count--;
+        }
+    }
+
+    public String toString() {
+        String str = "";
+        for (Node<T> aux = this.head; aux != null; aux = aux.getNext()) {
+            str += aux + ", ";
+        }
+        return str;
+    }
+
+}
